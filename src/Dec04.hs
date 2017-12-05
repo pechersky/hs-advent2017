88b7bb919542141f5f090491b51591cb0c085d53
Yakov Pechersky <ffxen158@gmail.com>
2017-12-05 01:03:17 -0500

    change names a bit to prevent clashes

--- src/Dec04.hs
+++ src/Dec04.hs
@@ -7,17 +7,17 @@ import Data.List
 day04file :: String
 day04file = "src/day04input1.txt"
 
-txtdata :: IO String
-txtdata = readFile day04file
+txtdata04 :: IO String
+txtdata04 = readFile day04file
 
-textlist :: IO [[String]]
-textlist = fmap (fmap words . lines) txtdata
+textlist04 :: IO [[String]]
+textlist04 = fmap (fmap words . lines) txtdata04
 
 -- nodupline checks if we've removed any duplicates
 nodupline :: [String] -> Bool
 nodupline line = (length line) == (length (nub line))
 
 -- @length . filter id@ gets the number of True in a list
-day04answer1 = fmap (length . filter id . fmap nodupline) textlist
+day04answer1 = fmap (length . filter id . fmap nodupline) textlist04
 -- @fmap (fmap sort)@ lexographically sorts each word, anagrams will become equal
-day04answer2 = fmap (length . filter id . fmap nodupline . fmap (fmap sort)) textlist
+day04answer2 = fmap (length . filter id . fmap nodupline . fmap (fmap sort)) textlist04
