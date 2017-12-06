7fbb7f3a55831373933a77ddd113d8bb45f3c9aa
Yakov Pechersky <ffxen158@gmail.com>
2017-12-06 02:31:27 -0500

    refactor out inner loop to redistribute, use [Int] as state storage

--- src/Dec06.hs
+++ src/Dec06.hs
@@ -25,9 +25,6 @@ memory = fmap (ixlist . intarr . head . cell . rows . T.pack) txtdata06
     intarr :: [T.Text] -> [Int]
     intarr = fmap (read . T.unpack)
 
-tostr :: [Int] -> String
-tostr = (intercalate " " . fmap show)
-
 ixlist :: [Int] -> M.Map Int Int
 ixlist = M.fromList . zip [0::Int ..]
 
@@ -40,36 +37,33 @@ getMax xmap = M.foldlWithKey keyCompare ((head . M.toList) xmap) xmap
       | oa < a = (k, a)
       | otherwise = (ok, oa)
 
-expandstates :: M.Map Int Int -> [String]
-expandstates xmap = go startval startpos [] xmap
+redistribute :: M.Map Int Int -> M.Map Int Int
+redistribute oldmap = newmap
   where
-    (startpos, startval) = getMax xmap
+    newmap = fold_ (const (+1)) (M.adjust (const 0) pos oldmap) keys
+    (pos, val) = getMax oldmap
+    fold_ = foldr . M.adjustWithKey
+    keys = [k `mod` M.size oldmap | (k, v) <- zip [(pos + 1) ..] [1 .. val]]
 
-    go :: Int -> Int -> [String] -> M.Map Int Int -> [String]
-    go !val !pos !pastlists !list
-      | strlist `elem` pastlists = pastlists ++ [strlist]
-      | otherwise = go newval newpos (pastlists ++ [strlist]) newlist
+-- use just the values for comparison for quick/cheap as opposed to comparing Maps
+type Output = [Int]
+expandstates :: M.Map Int Int -> [Output]
+expandstates xmap = go [] xmap
+  where
+    go :: [Output] -> M.Map Int Int -> [Output]
+    go !paststates !oldmap
+      | state `elem` paststates = paststates ++ [state]
+      | otherwise = go (paststates ++ [state]) newmap
         where
-          newlist = loop val pos (M.adjust (const 0) pos list)
-          (newpos, newval) = getMax newlist
-          strlist = tostr (M.elems list)
-
-          loop :: Int -> Int -> M.Map Int Int -> M.Map Int Int
-          loop !ival !ipos !ilist
-            | ival == 0 = ilist
-            | otherwise = loop (ival - 1) jpos (M.adjust (+1) jpos ilist)
-            where
-              jpos = (ipos + 1) `mod` M.size ilist
-
+          newmap = redistribute oldmap
+          state = (M.elems oldmap)
 
 day06answer1 = do
   xmap <- memory
-  let (pos, val) = getMax xmap
   return $ (subtract 1) . length $ expandstates xmap
 
 day06answer2 = do
   xmap <- memory
-  let (pos, val) = getMax xmap
   let statelist = expandstates xmap
   let laststate = head (reverse statelist)
   return $ (subtract 1) . length $ dropWhile (/= laststate) statelist
