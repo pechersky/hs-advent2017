741af0b645c3c9419623319fca3011748111c44e
Yakov Pechersky <ffxen158@gmail.com>
2017-12-13 01:43:14 -0500

    highly simplified dec13 solution

--- src/Dec13.hs
+++ src/Dec13.hs
@@ -26,41 +26,28 @@ type Depth = Int
 type Range = Int
 
 parseInput :: [String] -> (Depth, Range)
-parseInput (x:y:xs) = (read x, read y)
-
-layers :: [(Depth, Range)] -> Int -> [Int]
-layers layerinfo time = fmap go layerinfo
-  where
-    go (depth, range) = scannerpos time
-      where
-        layer range = [0 .. range - 1] ++ (reverse [0 .. range - 2])
-        scannerpos time = (layer range) !! (time `mod` (2*(range - 1)))
+parseInput (x:y:_) = (read x, read y)
 
 cost :: Int -> [(Depth, Range)] -> Int
-cost offset layerinfo = sum scannerhit
+cost offset layerinfo =  sum . fmap (uncurry (*) . fst) . filter ((== 0) . snd) . zip layerinfo $ modpos
   where
-    maxdepth = maximum (fst <$> layerinfo)
-    traveler = [offset .. maxdepth + offset]
-    scannerpos = (layers layerinfo <$> traveler)
-    scannerhit = fmap go (zip layerinfo [0 ..])
-      where
-        go ((depth, range), numlayer)
-          | ((scannerpos !! depth) !! numlayer) == 0 = depth*range
-          | otherwise = 0
+    depths = fmap fst layerinfo
+    ranges = fmap snd layerinfo
+    cycles = fmap ((*2) . (subtract 1)) ranges
+    modpos = zipWith mod (fmap (+ offset) depths) cycles
 
-
-seekcost layerinfo = fst . head . filter (hit . diag . snd) $ zip times $ fmap scannerpos times
+-- a seekcost that called cost to filter on
+-- @cost offset layerinfo == 0@ took 2 minutes
+seekcost :: [(Depth, Range)] -> Int
+seekcost layerinfo = fst . head . filter (hit . snd) . zip times . fmap modpos $ times
   where
-    times = [0 ..]
-    maxdepth = maximum (fst <$> layerinfo)
-    diag = zipWith ($) (fmap (flip (!!)) [0 ..])
+    depths = fmap fst layerinfo
+    ranges = fmap snd layerinfo
+    cycles = fmap ((*2) . (subtract 1)) ranges
+    modpos offset = zipWith mod (fmap (+ offset) depths) cycles
     hit :: [Int] -> Bool
     hit = all (/= 0)
-    scannerpos time = fmap go layerinfo
-      where
-        go (depth, range) = layers layerinfo (time + depth)
-
-
+    times = [0..]
 
 day13answer1 = do
   input <- minput
