18efb47904006338ffe999dd983ebce2b2cafd4e
Yakov Pechersky <ffxen158@gmail.com>
2017-12-15 02:05:21 -0500

    add dec15 typesigs

--- src/Dec15.hs
+++ src/Dec15.hs
@@ -6,12 +6,15 @@ input :: (Integer, Integer)
 {-input = (65, 8921)-}
 input = (591, 393)
 
+step :: (Integer, Integer) -> (Integer, Integer)
 step (x,y) = ((x*16807) `rem` val, (y*48271) `rem` val)
   where
     val = 2147483647
 
+check :: (Integer, Integer) -> Bool
 check (x,y) = all id [testBit x i == testBit y i | i <- [0..15]]
 
+newlists :: [(Integer, Integer)]
 newlists = zip (((filter ((== 0) . (flip rem) 4)) . fmap fst) list) (((filter ((== 0) . (flip rem) 8)) . fmap snd) list)
   where
     list = iterate step input
