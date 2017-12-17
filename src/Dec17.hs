b3e59923770df5491e952b72a6fe9cf7f9fb4845
Yakov Pechersky <ffxen158@gmail.com>
2017-12-17 01:35:25 -0500

    cleaned up dec17

--- src/Dec17.hs
+++ src/Dec17.hs
@@ -5,23 +5,18 @@ import Data.Maybe
 
 movelen = 356
 
-go move !list !pos !inv = ((newlist, newpos), inv+1)
+runSpinlock= iterate (go movelen)
   where
-    lenlist = inv
-    splitpos = (pos + move) `mod` lenlist
-    (axs,bxs) = splitAt (splitpos + 1) list
-    newlist = axs ++ [inv] ++ bxs
-    newpos = splitpos + 1
+    go move ((!list, !pos), !inv) = ((newlist, newpos), inv+1)
+      where
+        lenlist = inv
+        splitpos = (pos + move) `mod` lenlist
+        (axs,bxs) = splitAt (splitpos + 1) list
+        newlist = axs ++ [inv] ++ bxs
+        newpos = splitpos + 1
 
-itf = iterate (uncurry (uncurry (go movelen)))
-
-x = itf (([0], 0), 1)
-
-y = fst . fst $ x !! 2017
-
-
-itf2 :: [Int] -> (Int, Int)
-itf2 = foldl' (shortgo movelen) (0, 1)
+shortSpinlock :: [Int] -> (Int, Int)
+shortSpinlock = foldl' (shortgo movelen) (0, 1)
   where
     shortgo move !(!prevpos, !pos) lenlist
       | newpos == 1 = (lenlist, newpos)
@@ -30,6 +25,9 @@ itf2 = foldl' (shortgo movelen) (0, 1)
         splitpos = (pos + move) `mod` lenlist
         newpos = splitpos + 1
 
-day17answer1 = y !! (1 + fromJust (elemIndex 2017 y))
+day17answer1 = spinlock!! (1 + fromJust (elemIndex 2017 spinlock))
+  where
+    spinlockStates = runSpinlock (([0], 0), 1)
+    spinlock = (fst . fst) (spinlockStates !! 2017)
 
-day17answer2 = fst $ itf2 [1..1+5e7]
+day17answer2 = fst $ shortSpinlock [1..1+5e7]
