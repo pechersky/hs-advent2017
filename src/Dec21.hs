3e6a0af014f6145b207059731bdf7f613023f679
Yakov Pechersky <ffxen158@gmail.com>
2017-12-21 01:35:40 -0500

    cleaner parsing and typesigs

--- src/Dec21.hs
+++ src/Dec21.hs
@@ -12,52 +12,53 @@ import AOCommon (Parser, parseLines)
 day21file :: String
 day21file = "src/day21input1.txt"
 
-parseInput :: Parser (String, String)
+type Grid = [String]
+
+parseInput :: Parser (Grid, Grid)
 parseInput = (,) <$> valid <* string " => " <*> valid
   where
-    valid :: Parser String
-    valid = many (oneOf (".#/" :: String))
+    valid :: Parser Grid
+    valid = some (oneOf (".#" :: String)) `sepBy1` "/"
+
+toMap :: [(Grid, Grid)] -> M.Map Grid Grid
+toMap = M.fromList . concat . fmap go
+  where
+    go (x, y) = [(x', y) | x' <- allRule x]
 
 startGrid = [".#.","..#","###"]
 
-breakBy2 :: M.Map [String] [String] -> [String] -> [String]
+breakBy2 :: M.Map Grid Grid -> Grid -> Grid
 breakBy2 map = concat . fmap (
                 fmap concat . transpose
               . fmap (map M.!)
               . transpose . fmap (LS.chunksOf 2)
               ) . LS.chunksOf 2
 
-breakBy3 :: M.Map [String] [String] -> [String] -> [String]
+breakBy3 :: M.Map Grid Grid -> Grid -> Grid
 breakBy3 map = concat . fmap (
                 fmap concat . transpose
               . fmap (map M.!)
               . transpose . fmap (LS.chunksOf 3)
               ) . LS.chunksOf 3
 
-breakGrid :: M.Map [String] [String] -> [String] -> [String]
+breakGrid :: M.Map Grid Grid -> Grid -> Grid
 breakGrid m grid
   | length grid `mod` 2 == 0 = breakBy2 m grid
   | otherwise = breakBy3 m grid
 
-toMap :: [(String, String)] -> M.Map [String] [String]
-toMap = M.fromList . concat . fmap go
-  where
-    go (go'->x, go'->y) = [(x', y) | x' <- allRule x]
-    go' = LS.splitOn "/"
-
-flipRule :: [String] -> [String]
+flipRule :: Grid -> Grid
 flipRule = fmap reverse
 
-rotateRule :: [String] -> [String]
+rotateRule :: Grid -> Grid
 rotateRule = flipRule . transpose
 
-allRule :: [String] -> [[String]]
+allRule :: Grid -> [Grid]
 allRule rule = do
   f <- [id, flipRule]
   g <- [id, rotateRule, rotateRule . rotateRule, rotateRule . rotateRule . rotateRule]
   return $ (g . f) rule
 
-pixelsOn :: [String] -> Int
+pixelsOn :: Grid -> Int
 pixelsOn = length . filter (== '#') . concat
 
 day21answer1 = do
