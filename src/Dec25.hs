080a1456e92fb4a077e6555d47d122f8532cab7e
Yakov Pechersky <ffxen158@gmail.com>
2017-12-25 04:39:46 -0500

    add typesigs, cleanup parsing, use mconcat

--- src/Dec25.hs
+++ src/Dec25.hs
@@ -4,65 +4,69 @@
 module Dec25 where
 
 import Data.List
-import qualified Data.List.Split as LS
 import Data.Char
 import qualified Data.Map.Strict as M
 import qualified Data.IntMap.Lazy as IM
-import qualified Data.Set as S
+import Control.Applicative
 
 import Text.Megaparsec
-import Text.Megaparsec.Perm
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer as L
 import AOCommon (Parser, parseInput)
 
-import Control.Monad
-import Control.Applicative
-import Control.Monad.Trans.State.Strict as ST
-import Control.Monad.Trans.Class
-import Data.Char
-import Data.Maybe
-import Data.Tuple
 
 day25file :: String
 day25file = "src/day25input1.txt"
 
-parseInstr :: Parser (Char, Int, MInstr)
-parseInstr = (,,) <$ "Begin in state " <*> anyChar
-                  <*> skipSomeTill anyChar decimal
-                  <*  skipSomeTill anyChar newline
-                  <*> (foldr M.union M.empty <$> some (parseMI <* "." <* (optional newline)))
-                  <*  eof
+type PState = Char
+parsePState = "state " *> anyChar
+
+type Cursor = Int
+
+type Value = Int
+parseValue = decimal
+
+data Move = MLeft | MRight
+  deriving Show
+parseMove = choice [MLeft <$ "left", MRight <$ "right"]
+
+skipTo :: Parser a -> Parser a
+skipTo = skipSomeTill (notChar '\n')
+
+type MInstr = M.Map PState (M.Map Value (Value, Move, PState))
+parseMInstr = M.singleton <$  newline
+                          <*  "In " <*> parsePState <* newline
+                          <*> (mconcat <$> some parseStep)
   where
-    parseMI :: Parser MInstr
-    parseMI = M.singleton <$ newline <* "In state " <*> anyChar
-                          <*> (M.union <$> parseMove <*> parseMove)
-    parseMove :: Parser (M.Map Int (Int, String, Char))
-    parseMove =  M.singleton <$> skipSomeTill anyChar decimal
+    parseStep :: Parser (M.Map Value (Value, Move, PState))
+    parseStep =  M.singleton <$> skipTo parseValue <* newline
                              <*> parseBlock
-    parseBlock :: Parser (Int, String, Char)
-    parseBlock = (,,) <$> skipSomeTill anyChar decimal
-                      <*> skipSomeTill anyChar (choice ["left", "right"])
-                      <*  skipSomeTill anyChar "state " <*> anyChar
+                             <*  optional newline
+    parseBlock :: Parser (Value, Move, PState)
+    parseBlock = (,,) <$> skipTo parseValue  <* newline
+                      <*> skipTo parseMove   <* newline
+                      <*> skipTo parsePState
 
+parseInstr :: Parser (PState, Int, MInstr)
+parseInstr = (,,) <$ "Begin in " <*> parsePState  <* newline
+                  <*> skipTo decimal <* skipTo newline
+                  <*> (mconcat <$> some parseMInstr)
+                  <*  eof
 
-type MInstr = M.Map Char (M.Map Int (Int, String, Char))
-
-runSet :: MInstr -> (Char, Int, IM.IntMap Int) -> (Char, Int, IM.IntMap Int)
+runSet :: MInstr -> (PState, Cursor, IM.IntMap Value) -> (PState, Cursor, IM.IntMap Value)
 runSet instructions (!state, !cursor, !tape) = (state', cursor', tape')
   where
     instr = instructions M.! state
     curr = IM.findWithDefault 0 cursor tape
-    (next, movestr, state') = instr M.! curr
+    (next, move, state') = instr M.! curr
     tape' = case next of
               0 -> IM.delete cursor tape
               _ -> IM.insert cursor next tape
-    cursor' = case movestr of
-                "left" -> cursor - 1
-                "right" -> cursor + 1
-                _ -> error "not a move"
+    cursor' = case move of
+                MLeft  -> cursor - 1
+                MRight -> cursor + 1
 
 day25answer1 = do
   input <- readFile day25file
-  (startState, numSteps, instrs) <- parseInput parseInstr input
+  (startState, numSteps, instrs) <- parseInput parseInstr $ filter (not . isPunctuation) input
   return $ sum . IM.elems . (\(_,_,x) -> x) . (!! numSteps) $ iterate (runSet instrs) (startState, 0, IM.empty)
